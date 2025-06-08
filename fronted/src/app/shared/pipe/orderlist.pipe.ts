91883bfd5496178c9021730c6998c0cc41f4c3ff
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-07 23:00:28 -0500

    Pipe Filtros

--- fronted/src/app/shared/pipe/orderlist.pipe.ts
+++ fronted/src/app/shared/pipe/orderlist.pipe.ts
@@ -6,13 +6,42 @@ import { TrackModel } from '@core/models/tracks.model';
 })
 export class OrderlistPipe implements PipeTransform {
 
-  transform(value: TrackModel[], args: string|null=null, sort:string='asc'): TrackModel[] {
+  // transform(value: TrackModel[], args: string|null=null, sort:string='asc'): TrackModel[] {
     
-    console.log('👉', value)
-    console.log('🎅', args)
-    console.log('👩‍💻', sort)
+  //   console.log('👉', value)
+  //   console.log('🎅', args)
+  //   console.log('👩‍💻', sort)
+  //   // Se retorna value para agregar el listado de canciones
+  //   return value;
+  // }
+
+  transform(value: Array<any>, args: string|null=null, sort:string='asc'): TrackModel[] {
     
-    return value;
+    try{
+      if(args==null){
+         return value
+      } else{
+        const tmpList = value.sort((a,b)=>{
+          if(a[args]<b[args]){
+            return -1
+          }
+          else if (a[args]==b[args]){
+            return 0
+          }
+          else if (a[args]> b[args]){
+            return 1;
+          }
+          return 1
+        });
+        return (sort=='asc')? tmpList : tmpList.reverse()
+      }
+
+    }catch(e){
+      console.log('Algo paso');
+      return value
+    }
+    
+    //return value;
   }
 
 }
